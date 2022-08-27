d27f3dbf29daa8f3b6d0bb130df250a4785f35e5
ejimenez123 <caribestic123@gmail.com>
2022-08-27 19:49:32 -0400

    deploy

---
+++ assets/guide_test-mode.md.924a9610.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as e,d as a}from"./app.82f05033.js";const y=JSON.parse('{"title":"Modo Prueba","description":"","frontmatter":{},"headers":[],"relativePath":"guide/test-mode.md"}'),o={name:"guide/test-mode.md"},p=a("",12),l=[p];function t(c,r,d,i,D,u){return e(),n("div",null,l)}const C=s(o,[["render",t]]);export{y as __pageData,C as default};
